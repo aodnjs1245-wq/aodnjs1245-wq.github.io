9d02b2f217f4937c4113201377141ece84bdd1d3
aodnjs1245-wq <aodnjs1245@gmail.com>
2026-01-30 14:15:46 +0900

    Update 01_supabase.js

--- js/01_supabase.js
+++ js/01_supabase.js
@@ -7,6 +7,6 @@
 if (!window.supabaseClient) {
   window.supabaseClient = supabase.createClient(
     "https://yhakzowipdzazebswmgx.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InloYWt6b3dpcGR6YXplYnN3bWd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3NDMyMDUsImV4cCI6MjA4NTMxOTIwNX0.Id3Lfhzpz9FG8jA7b9xkKXrfiZXu2VvvOX7JDpgdrgw"
+    "sb_publishable_YMxUFWADxfI2wSmMU7RDew_Q6nMYnR2"
   );
 }
