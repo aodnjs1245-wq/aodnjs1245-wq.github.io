34009b96d9fc8ab26e88dc4e3dfbeb5d727e561c
aodnjs1245-wq <aodnjs1245@gmail.com>
2026-05-20 16:40:24 +0900

    Update 01_supabase.js

--- js/01_supabase.js
+++ js/01_supabase.js
@@ -6,7 +6,7 @@
 
 if (!window.supabaseClient) {
   window.supabaseClient = supabase.createClient(
-    "https://wnaezdaizoildoyuirqe.supabase.co/rest/v1/",
+    "https://wnaezdaizoildoyuirqe.supabase.co",
     "sb_publishable_m9OudVQBw4tvPC0JFieUXg_B8T5Iww1"
   );
 }
